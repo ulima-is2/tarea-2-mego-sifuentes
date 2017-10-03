a510ef9188ec6e7b458b5e9bc44163487dacac22
DiegoMego <30615834+DiegoMego@users.noreply.github.com>
2017-10-02 23:58:42 -0500

    Update patrones.py

--- patrones.py
+++ patrones.py
@@ -1,67 +1,62 @@
 import sys
 
+class Gestor:
+    def __init__(self):
+        self.datos = []
+        self.entradas = []
+
+    def add(self, cine,pelicula_id, pelicula, funciones):
+        self.datos.append([cine, Pelicula(pelicula_id, pelicula), funciones])
+
+    def listar_peliculas(self, cine):
+        for dato in self.datos:
+            if dato[0].nombre == cine:
+                print("{} {}".format(dato[1].id, dato[1].nombre))
+
+    def listar_funciones(self, pelicula_id, cine):
+        for dato in self.datos:
+            if str(dato[1].id) == pelicula_id and dato[0].nombre == cine:
+                for funcion in dato[2]:
+                    print(funcion)
+    def guardar_entrada(self, pelicula_id, funcion_elegida, cantidad, cine):
+        self.entradas.append(Entrada(pelicula_id, funcion_elegida, cantidad, cine))
+
 class Entrada:
-    def __init__(self, pelicula_id, funcion, cantidad):
+    def __init__(self, pelicula_id, funcion, cantidad, cine):
         self.pelicula_id = pelicula_id
         self.funcion = funcion
         self.cantidad = cantidad
+        self.cine = cine
 
 class Pelicula:
     def __init__(self, id, nombre):
         self.id = id
         self.nombre = nombre
 
+    def set_nombre(self, nombre):
+        self.nombre = nombre
+
+class CineFactory:
+    def get_cine(self, nombre):
+        if nombre == "CinePlaneta":
+            return CinePlaneta(nombre)
+        elif nombre == "CineStark":
+            return CineStark(nombre)
+        else:
+            return None
 
 class CinePlaneta:
-    def __init__(self):
-        peliculaIT = Pelicula(1, 'IT')
-        peliculaHF = Pelicula(2, 'La Hora Final')
-        peliculaD = Pelicula(3, 'Desparecido')
-        peliculaDeep = Pelicula(4, 'Deep El Pulpo')
-
-        peliculaIT.funciones = ['19:00', '20.30', '22:00']
-        peliculaHF.funciones = ['21:00']
-        peliculaD.funciones = ['20:00', '23:00']
-        peliculaDeep.funciones = ['16:00']
-
-        self.lista_peliculas = [peliculaIT, peliculaHF, peliculaD, peliculaDeep]
-        self.entradas = []
-
-    def listar_peliculas(self):
-        return self.lista_peliculas
-
-    def listar_funciones(self, pelicula_id):
-        return self.lista_peliculas[int(pelicula_id) - 1].funciones
-
-    def guardar_entrada(self, id_pelicula_elegida, funcion_elegida, cantidad):
-        self.entradas.append(Entrada(id_pelicula_elegida, funcion_elegida, cantidad))
-        return len(self.entradas)
-
-
+    def __init__(self, nombre):
+        self.nombre = nombre
 
 class CineStark:
-    def __init__(self):
-        peliculaD = Pelicula(1, 'Desparecido')
-        peliculaDeep = Pelicula(2, 'Deep El Pulpo')
-
-        peliculaD.funciones = ['21:00', '23:00']
-        peliculaDeep.funciones = ['16:00', '20:00']
-
-        self.lista_peliculas = [peliculaD, peliculaDeep]
-        self.entradas = []
-
-
-    def listar_peliculas(self):
-        return self.lista_peliculas
-
-    def listar_funciones(self, pelicula_id):
-        return self.lista_peliculas[int(pelicula_id) - 1].funciones
-
-    def guardar_entrada(self, id_pelicula_elegida, funcion_elegida, cantidad):
-        self.entradas.append(Entrada(id_pelicula_elegida, funcion_elegida, cantidad))
-        return len(self.entradas)
+    def __init__(self, nombre):
+        self.nombre = nombre
 
 def main():
+    factory = CineFactory()
+    gestor = Gestor()
+    gestor.add(factory.get_cine("CinePlaneta"), 1, "Desaparecido", ["20:00", "23:00"])
     terminado = False;
     while not terminado:
         print('Ingrese la opción que desea realizar')
@@ -88,14 +83,12 @@ def main():
             cine = input('Primero elija un cine:')
             if cine == '1':
                 # CinePlaneta
-                cine = CinePlaneta()
+                cine = "CinePlaneta"
             elif cine == '2':
-                cine = CineStark()
+                cine = "CineStark"
 
-            peliculas = cine.listar_peliculas()
             print('********************')
-            for pelicula in peliculas:
-                print("{}. {}".format(pelicula.id, pelicula.nombre))
+            gestor.listar_peliculas(cine)
             print('********************')
 
         elif opcion == '3':
@@ -108,21 +101,19 @@ def main():
             cine = input('Primero elija un cine:')
             if cine == '1':
                 # CinePlaneta
-                cine = CinePlaneta()
+                cine = "CinePlaneta"
             elif cine == '2':
-                cine = CineStark()
+                cine = "CineStark"
 
-            peliculas = cine.listar_peliculas()
-            for pelicula in peliculas:
-                print("{}. {}".format(pelicula.id, pelicula.nombre))
+            gestor.listar_peliculas(cine)
             pelicula_elegida = input('Elija pelicula:')
             #pelicula = obtener_pelicula(id_pelicula)
             print('Ahora elija la función (debe ingresar el formato hh:mm): ')
-            for funcion in cine.listar_funciones(pelicula_elegida):
-                print('Función: {}'.format(funcion))
+            gestor.listar_funciones(pelicula_elegida, cine)
             funcion_elegida = input('Funcion:')
             cantidad_entradas = input('Ingrese cantidad de entradas: ')
-            codigo_entrada = cine.guardar_entrada(pelicula_elegida, funcion_elegida, cantidad_entradas)
+            codigo_entrada = str(pelicula_elegida) + str("2017") + str(pelicula_elegida)
+            gestor.guardar_entrada(pelicula_elegida, funcion_elegida, cantidad_entradas, cine)
             print('Se realizó la compra de la entrada. Código es {}'.format(codigo_entrada))
         elif opcion == '0':
             terminado = True
